5d0db0d7da49f9db4aaa8c13ca47947b0223ae52
sam <95609691+sam337amen@users.noreply.github.com>
2024-08-06 09:46:55 +0300

    javascript  task
    exercising JavaScript

---
+++ task-1.js
@@ -0,0 +1,13 @@
+// Task 1: Variables and Datatypes
+
+function main(){
+    const name = "Sam";
+    const age = 25;
+    const isStudent = false; 
+    
+    console.log("Name:", name);
+    console.log("Age:", age);
+    console.log("Is a student?:", isStudent);
+}
+
+main();
